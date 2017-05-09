1517ca1ac82534cdabcac71cc213030c7c0f7cbc
Enrico Rotundo <enrico.rotundo@gmail.com>
2017-05-09 10:55:54 +0200

    AA deataset builder done

--- Analysis/AA_dataset_builder.py
+++ Analysis/AA_dataset_builder.py
@@ -10,17 +10,24 @@ import pandas as pd
 import dask.dataframe as ddf
 
 from Utils import settings_binaryBestAnswer as settings
-
+from Utils.commons import prepare_folder
 
 def main():
     df_feats = ddf.read_csv(settings.OUTPUT_PATH_DIR + 'features-*.csv', encoding=settings.ENCODING)
     df_ann = pd.read_csv(settings.ANNOTATION_CSV, encoding=settings.ENCODING)
 
-
+    df_feats = df_feats.compute()
     df_ann.set_index('id', inplace=True)
+    df_feats.set_index('post_id', inplace=True)
 
-    print df_ann.head()
+    df_merge = df_ann.join(df_feats, how='left')
+    df_merge.drop('Unnamed: 0', axis=1, inplace=True) # drop weird column
 
+    print df_merge.describe()
+
+    prepare_folder(settings.OUTPUT_PATH_DIR_AA_DATASET)
+
+    df_merge.to_csv(settings.OUTPUT_PATH_DIR_AA_DATASET  + '/AA_annotated_dataset.csv', encoding=settings.ENCODING)
 
 if __name__ == "__main__":
     main()
