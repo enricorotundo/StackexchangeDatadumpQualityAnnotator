17a3481739843b7bca85e1015c8dab8c002b6307
Enrico Rotundo <enrico.rotundo@gmail.com>
2017-05-10 14:42:47 +0200

    up

--- Analysis/Utils/settings_binaryBestAnswer.py
+++ Analysis/Utils/settings_binaryBestAnswer.py
@@ -5,15 +5,28 @@ import logging
 DB = 'travel'
 TASK_NAME = 'binaryBestAnswer'
 DATA_DIR_PATH = 'Analysis/Data/' + DB
-#SRC_FILE_NAME = 'threads_acceptedOnly_ansCountGte4.json'
-SRC_FILE_NAME = 'threads_acceptedOnly_all.json'
+SRC_FILE_NAME = 'threads_acceptedOnly_ansCountGte4.json'
+
+# JSON src file
+#SRC_FILE_NAME = 'threads_acceptedOnly_all.json'
 SRC_FILE_PATH = DATA_DIR_PATH + '/' + SRC_FILE_NAME
+
+# features
 OUTPUT_PATH_DIR = DATA_DIR_PATH + '/features_{}_{}/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
 OUTPUT_PATH_DIR_DRAFT = DATA_DIR_PATH + '/features_{}_{}_draft/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
+
+# dev/eval splitted features
 OUTPUT_PATH_DIR_SPLITTED = DATA_DIR_PATH + '/split_{}_{}/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
 OUTPUT_PATH_DIR_SPLITTED_DRAFT = DATA_DIR_PATH + '/split_{}_{}_draft/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
+
+# pre-processed features
 OUTPUT_PATH_DIR_PREPROC = DATA_DIR_PATH + '/preprocessed_{}_{}/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
 OUTPUT_PATH_DIR_PREPROC_DRAFT = DATA_DIR_PATH + '/preprocessed_{}_{}_draft/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
+
+# predictions
+OUTPUT_PATH_DIR_PREDICTIONS = DATA_DIR_PATH + '/predictions_{}_{}/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
+OUTPUT_PATH_DIR_PREDICTIONS_DRAFT = DATA_DIR_PATH + '/predictions_{}_{}_draft/'.format(SRC_FILE_NAME.split(".")[0], TASK_NAME)
+
 OUTPUT_PATH_DIR_AA_DATASET = DATA_DIR_PATH + '/AA_annotated_dataset/'
 ANNOTATION_CSV = DATA_DIR_PATH + '/' + 'travel_Posts_2017-04-04T09-06-50.604474_sheetV4.csv'
 ENCODING = 'utf-8'
--- Analysis/train_binaryBestAnswer.py
+++ Analysis/train_binaryBestAnswer.py
@@ -9,10 +9,12 @@ Evaluate over the evaluation set (unseen, left-out).
 
 import logging
 import argparse
+import pprint
 
 import dask
 import dask.multiprocessing
 import dask.dataframe as ddf
+import pandas as pd
 
 import dask_searchcv
 from sklearn.ensemble import RandomForestClassifier
@@ -22,6 +24,7 @@ from dask.diagnostics import ProgressBar
 
 from Metrics import ndcg
 from Utils import settings_binaryBestAnswer as settings
+from Utils.commons import prepare_folder
 
 dask.set_options(get=dask.multiprocessing.get)
 logging.basicConfig(format=settings.LOGGING_FORMAT, level=settings.LOGGING_LEVEL)
@@ -37,19 +40,24 @@ def main():
     with ProgressBar(dt=settings.PROGRESS_BAR_DT, minimum=settings.PROGRESS_BAR_MIN):
 
         if args.draft:
-            df_development = ddf.read_csv(settings.OUTPUT_PATH_DIR_SPLITTED_DRAFT + 'development-*.csv',
+            df_development = ddf.read_csv(settings.OUTPUT_PATH_DIR_PREPROC_DRAFT + 'development-*.csv',
                                           encoding=settings.ENCODING)
-            df_evaluation = ddf.read_csv(settings.OUTPUT_PATH_DIR_SPLITTED_DRAFT + 'evaluation-*.csv',
+            df_evaluation = ddf.read_csv(settings.OUTPUT_PATH_DIR_PREPROC_DRAFT + 'evaluation-*.csv',
                                          encoding=settings.ENCODING)
         else:
-            df_development = ddf.read_csv(settings.OUTPUT_PATH_DIR_SPLITTED + 'development-*.csv',
+            df_development = ddf.read_csv(settings.OUTPUT_PATH_DIR_PREPROC + 'development-*.csv',
                                           encoding=settings.ENCODING)
-            df_evaluation = ddf.read_csv(settings.OUTPUT_PATH_DIR_SPLITTED + 'evaluation-*.csv',
+            df_evaluation = ddf.read_csv(settings.OUTPUT_PATH_DIR_PREPROC + 'evaluation-*.csv',
                                          encoding=settings.ENCODING)
+
+        logging.info(pprint.pprint(sorted(df_development.columns)))
 
         X_development = df_development \
             .drop('best_answer', axis=1) \
             .drop('thread_id', axis=1) \
+            .drop('post_id', axis=1) \
+            .drop('Unnamed: 0', axis=1) \
+            .drop('Unnamed: 0.1', axis=1) \
             .drop('index', axis=1).compute()
         y_development = df_development['best_answer'].compute()
         groups_development = df_development['thread_id'].compute()
@@ -57,6 +65,9 @@ def main():
         X_evaluation = df_evaluation \
             .drop('best_answer', axis=1) \
             .drop('thread_id', axis=1) \
+            .drop('post_id', axis=1) \
+            .drop('Unnamed: 0', axis=1) \
+            .drop('Unnamed: 0.1', axis=1) \
             .drop('index', axis=1).compute()
         y_evaluation = df_evaluation['best_answer'].compute()
 
@@ -104,12 +115,25 @@ def main():
         df_predictions = dask.dataframe.concat([y_evaluation, y_predictions, groups], axis=1)
         df_predictions.columns = ['y_true', 'y_pred', 'thread_id']
 
+        # TODO print more predictions
+
         def compute_metrics(df):
+            # TODO check this
             return ndcg.ndcg_at_k(df['y_pred'], 1)
 
         # FIXME df_predictions['thread_id'] contains NaNs
         # FIXME dask.async.ValueError: cannot reindex from a duplicate axis
         ndcg_list = df_predictions.groupby('thread_id').apply(compute_metrics, meta=('x', 'f8')).compute()
+
+        # save predictions
+        if args.draft:
+            prepare_folder(settings.OUTPUT_PATH_DIR_PREDICTIONS_DRAFT)
+            df_predictions.compute().to_csv(settings.OUTPUT_PATH_DIR_PREDICTIONS_DRAFT + 'predictions.csv',
+                                            encoding=settings.ENCODING)
+        else:
+            prepare_folder(settings.OUTPUT_PATH_DIR_PREDICTIONS)
+            df_predictions.compute().to_csv(settings.OUTPUT_PATH_DIR_PREDICTIONS + 'predictions.csv',
+                                            encoding=settings.ENCODING)
 
         logging.info('nDCG@1: {}'.format(ndcg_list.mean()))
 
