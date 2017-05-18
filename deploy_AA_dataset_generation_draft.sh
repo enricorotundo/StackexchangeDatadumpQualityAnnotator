f8c2f3e9686c5c9f27f8d3b7219bae1344dff11b
Enrico Rotundo <enrico.rotundo@gmail.com>
2017-05-18 15:37:09 +0200

    draft

---
+++ deploy_AA_dataset_generation_draft.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# activate local virtualenv
+source venv/bin/activate &&
+
+# ETL from DB to local JSON files
+#python manage.py ETL_stack_threads &&
+#python manage.py ETL_stack_cooccurrence_network &&
+#python manage.py ETL_stack_ARN_network &&
+#python manage.py ETL_stack_ABAN_network &&
+#python manage.py ETL_stack_CBEN_network &&
+#python manage.py ETL_stack_users &&
+
+N_PARTITIONS=4
+DB='travel'
+TASK_NAME='binaryBestAnswer'
+SRC_FILE_NAME='threads_all_all.json'
+
+python -m Analysis.feats_extract --draft --n_partitions ${N_PARTITIONS} --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME} &&
+python -m Analysis.AA_dataset_builder
