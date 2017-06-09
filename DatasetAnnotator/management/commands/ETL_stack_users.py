c04e0042adcff4234bf3c0754480662ec581fa60
Enrico Rotundo <enrico.rotundo@gmail.com>
2017-06-09 13:20:36 +0200

    added nr_bestanswers

--- DatasetAnnotator/management/commands/ETL_stack_users.py
+++ DatasetAnnotator/management/commands/ETL_stack_users.py
@@ -28,17 +28,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """
-        Extract users data to JSON file
+        Extract users' data to JSON file
         """
+        users_dict = dict()  # stores the output
+
         # get all users, not just those in threads with accepted answer
         users = Posts.objects.using(db)\
+            .all()\
             .filter(posttypeid__in=[1, 2])\
             .values('owneruserid')\
-            .annotate(posts=Count('owneruserid'))\
-            .values('owneruserid', 'posts')
+            .annotate(nr_posts=Count('owneruserid'))\
+            .values('owneruserid', 'nr_posts')
 
-        users_dict = dict()
-        foo_ = [users_dict.update({d['owneruserid']: {u'nr_posts': d['posts']}}) for d in users]
+        # count the nr of posts for each user
+        _ = [users_dict.update({d['owneruserid']: {u'nr_posts': d['nr_posts']}}) for d in users]
+
+        accepted_answer_ids = Posts.objects.using(db) \
+            .all() \
+            .filter(posttypeid__in=[1, 2]) \
+            .values_list('acceptedanswerid') \
+            .distinct()
+
+        # count the nr of accepted answers for each user
+        users_w_best_answers = Posts.objects.using(db)\
+            .all() \
+            .filter(posttypeid__in=[1, 2], id__in=accepted_answer_ids) \
+            .values('owneruserid') \
+            .annotate(nr_bestanswers=Count('owneruserid'))
+
+        _ = [users_dict[d['owneruserid']].update({u'nr_bestanswers': d['nr_bestanswers']}) for d in users_w_best_answers]
+
 
         with io.open(OUTPUT_PATH + FILE_NAME, 'w', encoding='utf-8') as f:
             f.write(json.dumps(users_dict, ensure_ascii=False))
