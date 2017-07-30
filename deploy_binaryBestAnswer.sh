5be87eabbda21fc9a5ff6f0d1b25045895904d4d
Enrico Rotundo <enrico.rotundo@gmail.com>
2017-07-30 11:18:58 +0200

    up

--- deploy_binaryBestAnswer.sh
+++ deploy_binaryBestAnswer.sh
@@ -8,7 +8,7 @@ SRC_FILE_NAME='threads_acceptedOnly_ansCountGte2.json'
 ### activate local virtualenv
 source venv/bin/activate &&
 
-### ETL from DB to local JSON files
+### ETL from DB to local JSON files (these need Django so run it in local)
 #python manage.py ETL_stack_threads &&
 #python manage.py ETL_stack_cooccurrence_network &&
 #python manage.py ETL_stack_ARN_network &&
@@ -22,6 +22,6 @@ source venv/bin/activate &&
 ### Run the thing
 python -m Analysis.feats_extract --n_partitions ${N_PARTITIONS} --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME} &&
 python -m Analysis.split --n_partitions ${N_PARTITIONS} --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME} &&
-python -m Analysis.preprocess --scaler robust --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME}
-python -m Analysis.train --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME}
+python -m Analysis.preprocess --scaler robust --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME} &&
+python -m Analysis.train --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME} &&
 python -m Analysis.test --db ${DB} --task_name ${TASK_NAME} --src_file_name ${SRC_FILE_NAME}
